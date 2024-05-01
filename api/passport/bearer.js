c6617f1efa72ae2661a52fa222f0a76e1259dae5
KestutisVaitiekunas <kestutis.vaitiekunas@stud.ktmc.lt>
2024-05-01 12:28:32 +0300

    bearer auth responding with message

--- api/passport/bearer.js
+++ api/passport/bearer.js
@@ -8,7 +8,7 @@ const bearerstrategy = new BearerStrategy(
         try {
             let [token_in_db] = await AuthModel.get_token(req.db, token);
             token_in_db = token_in_db[0];
-            if (!token_in_db) return done(null, false, { message: 'Access denied. Invalid token' });
+            if (!token_in_db) return done(null, false, { message: 'Access denied' });
             return done(null, token_in_db.user_id);
         } catch (error) {
             console.log(error, false, { message: 'Internal server error', data: error });
@@ -24,8 +24,11 @@ const passport_authenticate = () => {
             console.log(err);
             console.log(user);
             console.log(info);
-            if (err) return res.status(500).json({ login_status: false, data: { message: info.message } });
-            if (!user) return res.status(401).json({ login_status: false, data: { message: info.message } });
+            if (err) return res.status(500).json({ auth_status: false, data: { message: info } });
+            if (!user) {
+                const parsed_info = info.split(",")[2].split("=")[1].replace(/\"/g, '');
+                return res.status(401).json({ auth_status: false, data: { message: parsed_info } });
+            }
             return res.status(200).json({auth_status: true, data: { user_id: user }});
         })(req, res, next);
     }
